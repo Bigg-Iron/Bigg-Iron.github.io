f3241710a9e58b9d6b64d73c46cfe4380af7aa47
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2018-03-20 17:37:04 -0700

    Fixed scroll behavior

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,30 +1,37 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-
-    console.log( "ready!" );
-
+$(document).ready(function(){
+    console.log(`app.js`)
     // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-   if (this.hash !== "") {
- 
-     // Prevent default anchor click behavior
-     event.preventDefault();
- 
-     // Store hash
-     var hash = this.hash;
- 
-     // Using jQuery's animate() method to add smooth page scroll
-     // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-     $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, 900, function(){
- 
-       // Add hash (#) to URL when done scrolling (default click behavior)
-       window.location.hash = hash;
-       });
-     } // End if
-   });
-
-});
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+        console.log(`click`);
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+    
+    $(window).scroll(function() {
+      $(".slideanim").each(function(){
+          console.log(`scrolling`);
+        var pos = $(this).offset().top;
+  
+        var winTop = $(window).scrollTop();
+          if (pos < winTop + 600) {
+            $(this).addClass("slide");
+          }
+      });
+    });
+  })
