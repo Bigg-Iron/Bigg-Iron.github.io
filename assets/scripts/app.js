59733086abd1d776b098feba4fe594a118cebfb3
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2018-03-25 16:09:14 -0700

    Added svg logo and  scroll to contact btn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   console.log(`app.js`);
   // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on("click", function(event) {
+  $(".navbar a, #about a, footer a[href='#myPage']").on("click", function(event) {
     console.log(`click`);
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
